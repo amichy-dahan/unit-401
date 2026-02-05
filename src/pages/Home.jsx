4b59e47d584f186c68c360ac4f5ea0d3ade22744
amichy-dahan <amichyd1@gmail.com>
2026-02-05 16:00:31 +0200

    trigger deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,56 +3,30 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./Home.css"
 import picLog from "../assets/46.png"
+import play from "../assets/play.png"
 
 function Home() {
   const [openModal, setOpenModal] = useState(null);
+    const driveLink = "https://drive.google.com/file/d/1AFrhlgefP580IqMWqFDwK23_Vw2Y5Z0l/view";
   return (
     <>
 
 
       <div className="pic">
-        <div className={`page ${openModal ? "blur" : ""}`}>
+       
           <img className="pic-home" src={picLog} alt="" />
-    
-        </div>
+
+         
+   <img
+  className="play"
+  src={play}
+  alt="סרטון"
+  style={{ cursor: "pointer" }} // חשוב שהסמן יראה שזה לחיץ
+  onClick={() => window.open(driveLink, "_blank", "noopener,noreferrer")}
+/>
+        
       </div>
-
-
-      {openModal && (
-        <div className="modal-overlay" onClick={() => setOpenModal(null)}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
-
-            {openModal === "one" && (
-              <>
-                <h3>(1967)<br></br>מלחמת ששת הימים </h3>
-                <p className="text-modal">
-                 גדוד 46 השתתף בכיבוש צומת רפיח, בשחרור רצועת עזה והשתתף בטיהור מחנות האויב. הגדוד נפגע בכיבוש אזור ביר־גפגפה.
-                </p>
-              </>
-            )}
-
-            {openModal === "two" && (
-              <>
-               <h3>(1968–1970)<br></br>מלחמת ההתשה</h3>
-                <p className="text-modal">
-                גדוד 46 פעל במרחב סואץ ובתעלת סואץ, והשתתף במבצעי "אסתמה" נגד בסיסי מחבלים וחדירות־מחבלים תוך לחימה מתמשכת.
-                </p>
-              </>
-            )}
-
-            {openModal === "three" && (
-              <>
-               <h3>(1973)<br></br> מלחמת יום הכיפורים</h3>
-                <p className="text-modal">
-              בימים הראשונים למלחמה לחם גדוד 46 בדרום תעלת סואץ, בגזרת המעוזים "בודפשט" ו"מצמד", עצר את צליחת הצבא המצרי וכבש מחדש מעוזים שנפלו. בהמשך השתתף הגדוד בצליחת התעלה והתקדם לעבר העיר סואץ.
-                </p>
-              </>
-            )}
-
-            <button onClick={() => setOpenModal(null)}>סגור</button>
-          </div>
-        </div>
-      )}
+ 
     </>
 
   )
