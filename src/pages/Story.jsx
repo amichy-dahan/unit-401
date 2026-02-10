cd5227043b7b33d8edb769ed3ff451178e2c449b
amichy-dahan <amichyd1@gmail.com>
2026-02-11 00:05:34 +0200

    trigger deploy

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -3,29 +3,22 @@
 
 import React from "react";
 import "./Story.css"
-import ReactPlayer from "react-player";
 function Story() {
     return (
         <>
-            <div className="pic">
-                <h1>סיפורי גבורה</h1>
+       <div className="pic">
+        <h1>סיפורי גבורה</h1>
 
-                <div className="video-wrapper">
-                    <ReactPlayer
-                        url="https://www.youtube.com/watch?v=1mFlpYijYjE&t=3s"
-                        controls={true}       // מציג את הכפתורים של יוטיוב
-                        width="100%"          // רספונסיבי לרוחב
-                        height="100%"         // רספונסיבי לגובה של div
-                        playing={false}       // לא מפעיל אוטומטית
-                        light={false}         // לא תמונה מוקדמת, אלא הסרטון
-                        config={{
-                            youtube: {
-                                playerVars: { playsinline: 1 } // חשוב למובייל
-                            }
-                        }}
-                    />
-                </div>
-            </div>
+        <div className="video-wrapper">
+          <iframe
+            src="https://www.youtube.com/embed/1mFlpYijYjE?start=3&playsinline=1"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
         </>
 
     )
