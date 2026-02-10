0aa6ea86dd928d1737467bfec6f192622f0bd8b9
amichy-dahan <amichyd1@gmail.com>
2026-02-10 23:54:21 +0200

    trigger deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,19 +16,6 @@ function Home() {
        
           <img className="pic-home" src={picLog} alt="" />
 
-         
-   <div style={{ position: "relative", paddingBottom: "56.25%", height: 0, overflow: "hidden" }}>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/1mFlpYijYjE?start=3&si=BYy8AW4_w_7IkP6T"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-        style={{ position: "absolute", top: 0, left: 0, width: "30%", height: "30%" }}
-      ></iframe>
-    </div>
         
       </div>
  
--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -3,24 +3,29 @@
 
 import React from "react";
 import "./Story.css"
+import ReactPlayer from "react-player";
 function Story() {
     return (
         <>
-         <div className="pic">
-           <h1>
-סיפורי גבורה
-      </h1>
+            <div className="pic">
+                <h1>סיפורי גבורה</h1>
 
-      <div className="video-wrapper">
-        <iframe
-          src="https://www.youtube.com/embed/1mFlpYijYjE?start=3"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-      </div>
-      </div>
+                <div className="video-wrapper">
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=1mFlpYijYjE&t=3s"
+                        controls={true}       // מציג את הכפתורים של יוטיוב
+                        width="100%"          // רספונסיבי לרוחב
+                        height="100%"         // רספונסיבי לגובה של div
+                        playing={false}       // לא מפעיל אוטומטית
+                        light={false}         // לא תמונה מוקדמת, אלא הסרטון
+                        config={{
+                            youtube: {
+                                playerVars: { playsinline: 1 } // חשוב למובייל
+                            }
+                        }}
+                    />
+                </div>
+            </div>
         </>
 
     )
