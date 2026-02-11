9eb28292799050dbb69705e8bba990de5fe54416
amichy-dahan <amichyd1@gmail.com>
2026-02-11 22:39:42 +0200

    trigger deploy

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -2,14 +2,21 @@
 
 
 import React from "react";
+import tank from "../assets/tank.png"
 import "./Story.css"
 function Story() {
-    return (
-        <>
-       <div className="pic">
-        <h1>סיפורי גבורה</h1>
+  return (
+    <>
+      <div className="pic">
 
-        <div className="video-wrapper">
+        <div className="pic-tank">
+ <img className="tank1" src={tank} alt="" />
+        </div>
+       
+        <h1 className="gvur">סיפורי גבורה</h1>
+    <div className="videos">
+
+   <div className="video-wrapper">
           <iframe
             src="https://www.youtube.com/embed/1mFlpYijYjE?start=3&playsinline=1"
             title="YouTube video player"
@@ -18,11 +25,21 @@ function Story() {
             allowFullScreen
           ></iframe>
         </div>
-      </div>
-        </>
 
-    )
+
+        <div className="video-wrapper">
+          
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/aPmuMD7fLmo?si=AOvVovgWAbqjgUcS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+
+    </div>
+     
+      </div>
+    </>
+
+  )
 
 }
 
 export default Story;
+
